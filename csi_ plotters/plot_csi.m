cd765124105286350682acb27c828c3865dc8213
Surya Prakash Koidala <51915675+Surya291@users.noreply.github.com>
2020-04-21 01:11:40 +0530

    Create plot_csi.m

---
+++ csi_ plotters/plot_csi.m
@@ -0,0 +1,15 @@
+function ret = plotcsi(csi_values,p)
+ret = zeros(30007,30);
+
+for i = 1:50
+  for j = 2
+csi_struct = csi_values{i};
+ret(i,j) = csi_struct.('csi')(1,1,j);
+ret(i,j) = abs(ret(i));
+ret(i,j) = 10 * log10(ret(i));
+
+
+
+end
+end
+end
